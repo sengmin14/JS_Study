9c8b4aeb55a568d39a345998ff1a83363646c648
진성민 <sengmin14@naver.com>
2023-12-19 18:25:07 +0900

    feat : ToDo-list 기능 추가 - 체크, 삭제 기능 추가 - 데이터 LocalStorage저장

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,15 +1,75 @@
 const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector('#todo-list');
+
+const savedTodoList = JSON.parse(localStorage.getItem('saved-items'));
+
+const createTodo = (storageData) => {
+    let todoContent = todoInput.value;
+    if (storageData) {
+        todoContent = storageData.contents;
+    }
+
+    const newLi = document.createElement('li');
+    const newSpan = document.createElement('span');
+    const newBtn = document.createElement('button');
+
+    newBtn.addEventListener('click', () => {
+        newLi.classList.toggle('complete');
+        saveItemsFn();
+    });
+
+    newLi.addEventListener('dblclick', () => {
+        newLi.remove();
+        saveItemsFn();
+    });
+
+    if (storageData?.complete === true) {
+        // storageData가 존재하지 않는데 complete를 참조하려 하기 때문에
+        // ? : undefine이거나 null이면 참조하지 않겠다.
+        newLi.classList.add('complete');
+    }
+
+    newSpan.innerHTML = todoContent;
+    newLi.appendChild(newBtn);
+    newLi.appendChild(newSpan);
+    todoList.appendChild(newLi);
+    todoInput.value = '';
+    saveItemsFn();
+    // console.log(todoList.children[0].querySelector('span').innerHTML);
+};
 
 const keyCodeCheck = () => {
     if (window.event.keyCode === 13 && todoInput.value !== '') {
-        const todoList = document.querySelector('#todo-list');
-
-        const newLi = document.createElement('li');
-        const newSpan = document.createElement('span');
-
-        newSpan.innerHTML = todoInput.value;
-        newLi.appendChild(newSpan);
-        todoList.appendChild(newLi);
-        todoInput.value = '';
+        createTodo();
     }
 };
+
+const deleteAll = () => {
+    const liList = document.querySelectorAll('li');
+    for (let i = 0; i < liList.length; i++) {
+        liList[i].remove();
+    }
+};
+
+const saveItemsFn = () => {
+    const saveItems = [];
+    for (let i = 0; i < todoList.children.length; i++) {
+        // console.log(todoList.children[0].querySelector('span').innerHTML);
+        const todoObj = {
+            contents: todoList.children[i].querySelector('span').innerHTML,
+            complete: todoList.children[i].classList.contains('complete'),
+        };
+        saveItems.push(todoObj);
+    }
+    if (saveItems.length === 0) {
+        localStorage.removeItem('saved-items');
+    } else {
+        localStorage.setItem('saved-items', JSON.stringify(saveItems));
+    }
+};
+
+if (savedTodoList) {
+    for (let i = 0; i < savedTodoList.length; i++) {
+        createTodo(savedTodoList[i]);
+    }
+}
